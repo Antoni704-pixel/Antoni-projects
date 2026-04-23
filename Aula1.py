f6482d00fae1d3b6b50a3e24b0da028b9cf57908
Antoni704-pixel <antoniofstedile@gmail.com>
2026-04-23 19:46:21 -0300

    meu primeiro código

---
+++ Aula1.py
@@ -0,0 +1,15 @@
+# isso é um comentário
+
+# o comando print serve para escrevermos alguma coisa
+
+print("olá mundo")
+print("esse é o curso de lógica de progamação")
+
+
+# O comando input() serve para receber uma entrada
+# dot teclado, sendo que usuaário irá digitar
+
+# A sintaxe do comando é input ("mensagem")
+nome = input("digite o seu nome: ")
+
+print("olá, ",nome)
